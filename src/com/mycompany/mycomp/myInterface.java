d0bae42eef6ea0b8577eb2aba2e7dcd0f39a2c2d
Jonathan Camacho <Jonathan Camacho@DESKTOP-4V427KT.resource.campus.njit.edu>
2016-10-19 20:12:40 -0400

    initial commit

---
+++ src/com/mycompany/mycomp/myInterface.java
@@ -0,0 +1,12 @@
+/**
+ * 
+ */
+package com.mycompany.mycomp;
+
+/**
+ * @author Jonathan Camacho
+ *
+ */
+public interface myInterface {
+
+}
---
+++ src/com/mycompany/mycomp/myclass1.java
@@ -0,0 +1,24 @@
+/**
+ * 
+ */
+package com.mycompany.mycomp;
+
+/**
+ * @author Jonathan Camacho
+ *
+ */
+public class myclass1 {
+	
+	private String name;
+	private Integer age;
+	protected String lastName;
+	
+	public String getName() {
+		return name;
+	}
+	
+	public String getSomething() {
+		return null;
+	}
+
+}
